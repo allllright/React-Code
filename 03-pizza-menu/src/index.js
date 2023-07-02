47874e99e5b895bd17043a5a00cae711d5f708d6
Liu <12333333zzzz@gmail.com>
2023-07-02 22:19:36 +0100

    07-02

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,14 +1,74 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
-  return <h1>Hello React</h1>;
+  return <h1>Hello React!</h1>;
 }
 
+// Component function name needs to start with uppercase letter and return some markup(JSX).
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/prosciutto.jpg" alt="a pizza"></img>
+      <h2>
+        Pizza Prosciutto
+        <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
+      </h2>
+    </div>
+  );
+}
 //React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    <Pizza></Pizza>
+    <Pizza></Pizza>
+  </div>
 );
