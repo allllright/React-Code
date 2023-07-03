e7437dea46baed09cb8ae4f0f54f77cd6c91111f
Liu <12333333zzzz@gmail.com>
2023-07-03 11:11:09 +0100

    07-03

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 const pizzaData = [
   {
     name: "Focaccia",
@@ -46,21 +47,95 @@ const pizzaData = [
 ];
 
 function App() {
-  return <h1>Hello React!</h1>;
-}
-
-// Component function name needs to start with uppercase letter and return some markup(JSX).
-function Pizza() {
   return (
-    <div>
-      <img src="pizzas/prosciutto.jpg" alt="a pizza"></img>
-      <h2>
-        Pizza Prosciutto
-        <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-      </h2>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
+function Header() {
+  // inline styling style={{ color: "red" }} first {}is to nter JS mode and sencond {} is to create an object
+  //   const style = { color: "red", fontSize: "32px" };
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>Still working on our menu, please come back later</p>
+      )}
+
+      {/* <Pizza
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        photoName="pizzas/prosciutto.jpg"
+        price={12}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, bread"
+        price={14}
+        photoName="pizzas/funghi.jpg"
+      /> */}
+    </main>
+  );
+}
+
+// props stands for properties
+function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt="a pizza"></img>
+      <h3>{props.pizzaObj.name} </h3>
+      <p>{props.pizzaObj.ingredients}</p>
+      <spain>{props.pizzaObj.price + 1}</spain>
+    </li>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  //   if (hour >= openHour && hour <= closeHour) alert("We are opening");
+  //   else alert("Sorry we're closed");
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <div className="order">
+          <p>We're open until {closeHour}:00</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We're open from {openHour}:00 until {closeHour}:00
+        </p>
+      )}
+    </footer>
+  );
+}
+// Component function name needs to start with uppercase letter and return some markup(JSX).
+
 //React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -68,7 +143,5 @@ root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-    <Pizza></Pizza>
-    <Pizza></Pizza>
   </div>
 );
