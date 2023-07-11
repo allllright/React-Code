d5d49ef1933bd5998717a78aa61b888c39bdb10e
Liu <12333333zzzz@gmail.com>
2023-07-11 22:31:19 +0100

    0711

--- 06-usepopcorn/src/index.js
+++ 06-usepopcorn/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+import StarRating from "./StarRating";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5} />
+    <StarRating maxRating={20} size={24} defaultRating={3} color={"blue"} />
+    {/* <App /> */}
   </React.StrictMode>
 );
-
