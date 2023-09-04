dad8176bfb7d18b81058dddafe3d69a8101f5601
Liu <12333333zzzz@gmail.com>
2023-09-04 22:37:47 +0100

    09-04

---
+++ 06-usepopcorn/src/useLocalStorageState.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  const [vaule, setVaule] = useState(function () {
+    const storeValue = localStorage.getItem("key");
+    return JSON.parse(storeValue);
+  });
+
+  // use localStorage
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(vaule));
+    },
+    [vaule, key]
+  );
+
+  return [vaule, setVaule];
+}
