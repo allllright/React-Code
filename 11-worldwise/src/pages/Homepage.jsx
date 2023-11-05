95a443beffeb0200b83ba57c650128e6cbc1d652
Liu <12333333zzzz@gmail.com>
2023-11-05 09:02:31 +0000

    1105

--- 11-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <>
       <PageNav />
+      <AppNav />
       <div>WorldWise</div>
       <Link to="/app">go to the app</Link>
     </>
